cb4c6f8cd9417379ca2954a1491c68771287d202
anas1qw2 <157681921+anas1qw2@users.noreply.github.com>
2024-08-05 09:08:54 +0530

    Update index.php

--- sem-project/index.php
+++ sem-project/index.php
@@ -28,32 +28,54 @@
     </main>
     <section class="blogCards">
         <?php
-            for($i=0;$i<27;$i++){
-                echo "<a href=\"blog.html\"><div class=\"card\">";
-                echo "<img class=\"cover\" src=\"demo.jpg\">";
-                echo "<h1 class=\"blogTitle\">Blog title</h1>";
-                echo "<p class=\"blogDes\">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores nostrum eligendi eveniet pariatur voluptates fuga quidem quod, qui nobis quibusdam unde, sapiente laudantium totam....</p>";
-               echo  "</div> </a>";
-               
+            // Array of blog posts
+            $blogs = [
+                [
+                    "title" => "Blog Title 1",
+                    "description" => "Description for blog 1...",
+                    "image" => "image1.jpg",
+                    "link" => "blog1.html"
+                ],
+                [
+                    "title" => "Blog Title 2",
+                    "description" => "Description for blog 2...",
+                    "image" => "image2.jpg",
+                    "link" => "blog2.html"
+                ],
+                [
+                    "title" => "Blog Title 3",
+                    "description" => "Description for blog 3...",
+                    "image" => "image3.jpg",
+                    "link" => "blog3.html"
+                ],
+                // Add more blogs here
+            ];
+
+            foreach ($blogs as $blog) {
+                echo "<a href=\"{$blog['link']}\"><div class=\"card\">";
+                echo "<img class=\"cover\" src=\"{$blog['image']}\">";
+                echo "<h1 class=\"blogTitle\">{$blog['title']}</h1>";
+                echo "<p class=\"blogDes\">{$blog['description']}</p>";
+                echo "</div> </a>";
             }
         ?> 
         </div>
     </section>
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 px-6 border-top">
-    <p class="col-md-4 mb-0 text-body-secondary">© 2024 Company, Inc</p>
+        <p class="col-md-4 mb-0 text-body-secondary">© 2024 Company, Inc</p>
 
-    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-    </a>
+        <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        </a>
 
-    <ul class="nav col-md-4 justify-content-end">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
-    </ul>
-  </footer>
+        <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+        </ul>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
